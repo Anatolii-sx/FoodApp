49974b13a848a1d15d43f990a42a9186f3618b7d
Anatolii-sx <s8.8@bk.ru>
2022-10-13 23:18:18 +0300

    #0 Initial commit

---
+++ FoodApp/App/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  FoodApp
+//
+//  Created by Анатолий Миронов on 13.10.2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
